211057632b0210ad8e74115e51208364ecd6bce9
Christopher Villamayor <villamayorchris71@gmail.com>
2025-06-21 16:34:31 -0300

    fix: Adiciona o @Column nas classes

--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,4 +1,4 @@
-package dev.java10x.CadastroDeNinjas.Ninjas.Model;
+package dev.java10x.CadastroDeNinjas.Ninjas;
 
 import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
